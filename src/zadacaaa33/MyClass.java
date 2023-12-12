4993ad945e9c96a9b40459621232b313c9b0b5a1
Jankulovska <jankulovskasara60@gmail.com>
2023-12-12 13:47:58 +0100

    Add first java program

---
+++ src/zadacaaa33/MyClass.java
@@ -0,0 +1,13 @@
+package zadacaaa33;
+
+public class MyClass {
+
+	public static void main(String[] args) {
+		   Vehicle v1 = new Vehicle("Red");
+	        System.out.println(v1);
+	        Vehicle v2 = new Vehicle();
+	        System.out.println(v2);
+
+	}
+
+}
---
+++ src/zadacaaa33/Vehicle.java
@@ -0,0 +1,16 @@
+package zadacaaa33;
+
+public class Vehicle {
+	private String color;
+	  Vehicle(String a) {
+	        color = a;
+	    }
+ Vehicle(){
+	 color="blue";
+ }
+	    @Override
+	    public String toString() {
+	        return color;
+	}
+
+}
